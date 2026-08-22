aad47c83b8ef3466be83bc242cbbc6ceab593dea
Karanam Bhuvanesh <155879554+bhuvan120@users.noreply.github.com>
2026-08-22 15:35:37 +0530

    Set basename for BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './index.css';
  */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Real-estate_project_assessment">
       <App />
     </BrowserRouter>
   </React.StrictMode>
